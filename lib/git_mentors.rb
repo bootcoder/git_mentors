b8347e561c9c2ffceaa4f847b6c40371f0965894
BootCoder <bootcoder@gmail.com>
2016-07-15 00:35:46 -0700

    MVP - find orgs - parse teams - add user - basic CLI

--- lib/git_mentors.rb
+++ lib/git_mentors.rb
@@ -1,9 +1,56 @@
+require_relative 'github_adapter'
+require 'pry-byebug'
+require 'awesome_print'
+require 'dotenv'
+Dotenv.load
+
 class GitMentors
 
   def initialize
+    @github = GithubAdapter.new(ENV['GIT_EMAIL'],ENV['GIT_PASSWORD'])
+    start_CLI
   end
 
-  def
+  def start_CLI
+    orgs = find_all_user_orgs
+    list_output(orgs)
+    input_org = find_org(select_prompt, orgs)
+    team_id = find_org_employees_team_id(input_org["login"])
+    ap add_user("DBC-SF", team_id)
+  end
 
+  private
+
+  def find_all_user_orgs
+    @github.get_orgs.reverse
+  end
+
+  def add_user(username, team_id)
+    @github.team_add_user(username, team_id)
+  end
+
+  def find_org(org_index, orgs)
+    orgs[org_index-1]
+  end
+
+  def find_org_employees_team_id(input_org)
+    teams = @github.get_org_teams(input_org).select{|team| team["name"] == "Employees"}
+    teams.first["id"] if !teams.empty? && teams.first["name"] == "Employees"
+  end
+
+
+  def list_output(arr)
+    arr.each_with_index do |item, index|
+      puts "#{index+1}: #{item['login']}"
+    end
+  end
+
+  def select_prompt
+    puts "Select Cohort (int)"
+    print "=> "
+    gets.chomp.to_i
+  end
 
 end
+
+git_mentors = GitMentors.new
--- lib/github_adapter.rb
+++ lib/github_adapter.rb
@@ -1,8 +1,32 @@
+require 'pry-byebug'
+require 'httparty'
+require 'awesome_print'
+
 class GithubAdapter
 
-  def initialize
+  include HTTParty
 
+  base_uri 'https://api.github.com'
+
+  def initialize(u, p)
+    @auth = {username: u, password: p}
   end
 
+  def test(endpoint)
+    self.class.get(endpoint, basic_auth: @auth)
+  end
+
+  def get_orgs
+    self.class.get("/user/orgs", {basic_auth: @auth, query: {per_page: 50}})
+  end
+
+  def get_org_teams(org)
+    self.class.get("/orgs/#{org}/teams", basic_auth: @auth)
+  end
+
+  def team_add_user(username, team_id)
+    # PUT /teams/:id/memberships/:username
+    self.class.put("/teams/#{team_id}/memberships/#{username}", basic_auth: @auth)
+  end
 
 end
